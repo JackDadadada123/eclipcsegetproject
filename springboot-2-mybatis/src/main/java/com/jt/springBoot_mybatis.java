3a70cb02eea430444a4cff5b17a299d452200faa
lj <408386551@qq.com>
2020-01-07 20:34:03 +0800

    新增

--- springboot-2-mybatis/src/main/java/com/jt/springBoot_mybatis.java
+++ springboot-2-mybatis/src/main/java/com/jt/springBoot_mybatis.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.jt.mapper")
-//abcd
+//abcd456
 public class springBoot_mybatis {
 	public static void main(String[] args) {
 		SpringApplication.run(springBoot_mybatis.class, args);
